26bad7f61c9d680732e71d8cf89db0ca201aa2c8
VLAD EMINOVICI <veminovici@hotmail.com>
2024-03-20 12:27:26 +0200

    Add penta minor scale

--- src/scales/minor.rs
+++ src/scales/minor.rs
@@ -6,15 +6,26 @@ pub fn minor(tonic: Note) -> Scales {
     Scales::minor_with_steps(" minor", tonic, steps.into_iter())
 }
 
+pub fn pentatonic_minor(tonic: Note) -> Scales {
+    let steps = [3, 2, 2, 3, 2];
+    Scales::minor_with_steps(" penta minor", tonic, steps.into_iter())
+}
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::C;
+    use crate::{A, C};
 
     #[test]
     fn test_minor() {
         let scale = minor(C);
         assert_eq!(format!("{scale:X}"), "C minor [C, D, D#, F, G, G#, A#, C]");
         assert_eq!(format!("{scale:x}"), "C minor [C, D, Eb, F, G, Ab, Bb, C]");
+    }
+
+    #[test]
+    fn test_penta_minor() {
+        let scale = pentatonic_minor(A);
+        assert_eq!(format!("{scale:X}"), "A penta minor [A, C, D, E, G, A]");
+        assert_eq!(format!("{scale:x}"), "A penta minor [A, C, D, E, G, A]");
     }
 }
